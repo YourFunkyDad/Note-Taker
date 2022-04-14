942067620f3b1335c16fb72507a081e535886538
Funky Dad <jacobmontgomeryallred@gmail.com>
2022-04-14 12:43:45 -0400

    Adding Route Requirements

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,11 +1,29 @@
-const express = require('express')
-const app = express()
+// Require Dependencies
+const express = require('express');
+const fs = require("fs");
+
+// Initialize Express
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+fs.readFile("./db/db.json","json",function(data) {
+  var notes = data;
+});
+
+// Require Routes
+require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
 
 let noteTitle;
 let noteText;
