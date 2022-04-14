eca8eca8525082a262ad517581a597d7e8d83492
Funky Dad <jacobmontgomeryallred@gmail.com>
2022-04-14 12:32:49 -0400

    Package-Lock

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -1,3 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
+
 let noteTitle;
 let noteText;
 let saveNoteBtn;
