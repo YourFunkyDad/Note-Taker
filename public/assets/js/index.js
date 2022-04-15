285d741fe4ee719c9b7554badad510422e80d488
Funky Dad <jacobmontgomeryallred@gmail.com>
2022-04-15 12:14:18 -0400

    Server and Route File Added

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,30 +1,3 @@
-// Require Dependencies
-const express = require('express');
-const fs = require("fs");
-
-// Initialize Express
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-fs.readFile("./db/db.json","json",function(data) {
-  var notes = data;
-});
-
-// Require Routes
-require('./routes/htmlRoutes')(app);
-require('./routes/apiRoutes')(app);
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
-});
-
 let noteTitle;
 let noteText;
 let saveNoteBtn;
